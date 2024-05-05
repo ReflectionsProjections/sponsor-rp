24efed7e5f22d3777cf1a82a449dac5b9be554c8
Aydan Pirani <aydanpirani@gmail.com>
2024-05-05 15:33:53 -0500

    redid stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,14 @@
 import { Box, Image } from "@chakra-ui/react";
 
 export function NavBar() {
-    return <Box height="100%" display="flex" alignItems="center" pl="10px" pt="10px" pb="10px">
-        {/* <a href="https://reflectionsprojections.org">         */}
+    return (
+        <Box height="100%" display="flex" alignItems="center" pl="10px" pt="10px" pb="10px">
             <Image
                 src="/white_rp_logo.svg"
                 maxH="100%"
                 _hover={{ filter: "brightness(20%)" }}  
                 onClick={() => {window.location.href = "https://reflectionsprojections.org"}}
             />
-        {/* </a> */}
-            
-    </Box>
+        </Box>
+    )
 }
