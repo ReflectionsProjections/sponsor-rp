658d1f23309209a4a6f90bfdad08ef6d676a3e80
Aydan Pirani <aydanpirani@gmail.com>
2024-05-05 15:21:57 -0500

    NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export function NavBar() {
     return <Box height="100%" display="flex" alignItems="center" pl="10px" pt="10px" pb="10px">
         {/* <a href="https://reflectionsprojections.org">         */}
             <Image
-                src="/white_rp_logo.svg"
+                src="white_rp_logo.svg"
                 maxH="100%"
                 _hover={{ filter: "brightness(20%)" }}  
                 onClick={() => {window.location.href = "https://reflectionsprojections.org"}}
